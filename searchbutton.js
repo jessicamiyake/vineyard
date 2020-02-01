d6aaae514d58d2d304d661e5ed95e4c0e67e6819
Jessica Miyake <jmiyake@wisc.edu>
2020-02-01 17:49:23 -0500

    angelic choir

--- searchbutton.js
+++ searchbutton.js
@@ -4,62 +4,66 @@ function search() {
     var v1 = {
         id: "1",
         url: "videos/junior.mp4",
-        tags: ["kids","pizza"],
+        tags: ["#kids","#pizza"],
         dialogue: "if your name is junior and you're really handsome come on raise your hand"
       };
-    var v2 = JSON.parse({
+    var v2 = {
         id: "2",
         url: "videos/knife.mp4",
-        tags: ["knife","kids"],
+        tags: ["#knife","#kids"],
         dialogue: "let me see what you have a knife no"
-      });
-    var v3 = JSON.parse({
+      };
+    var v3 = {
         id: "3",
         url: "videos/giraffe.mp4",
-        tags: ["giraffe","colorado"],
+        tags: ["#giraffe","#colorado"],
         dialogue: "say colorado i'm a giraffe"
-      });
-    var v4 = JSON.parse({
+      };
+    var v4 = {
         id: "4",
         url: "videos/arrow.mp4",
         tags: [],
         dialogue: ""
-      });
-    var v5 = JSON.parse({
+      };
+    var v5 = {
         id: "5",
         url: "videos/light.mp4",
         tags: [],
         dialogue: "kevin don't kevin kevin kevin kevin watch the light dude"
-      });
-    var v6 = ({
+      };
+    var v6 = {
         id: "6",
         url: "videos/chickens.mp4",
         tags: [],
         dialogue: "look at all those chickens"
-      });
+      };
     var vines = [v1,v2,v3,v4,v5,v6];
     var results = [];
 
     var input = document.getElementById('searchtext').value;
+    console.log(input);
     var isTag = input.startsWith("#");
     input.toLowerCase();
-    var searchtext = input.split();
+    var searchtext = input.split(" ");
+    console.log(searchtext);
     if (isTag) {
-        for (var j = 0; j < searchtext.size(); j++) {
-            searchtext[j] = searchtext[j].subStr(1);
-            for (var k = 0; k < vines.size(); k++) {
-                vines[k].tags.array.forEach(tag => {
+        for (var j = 0; j < searchtext.length; j++) {
+            //searchtext[j] = searchtext[j].subString(1);
+            for (var k = 0; k < vines.length; k++) {
+                vines[k].tags.forEach(tag => {
                     if (searchtext[j] === tag && !results.includes(vines[k])) {
-                        console.log(tag);
+                        console.log(vines[k].url);
                         results.push(vines[k]);
                     }
                 });
             }
         }
+    } else {
+
     }
-    // for (var i = 0; i < results.size(); i++) {
-    //     var vineresults.innerHTML = "<video><source src=\"" + results[i].url + "\"type=\"video/mp4\"></video><br>";
-    //     console.log(results[i].url);
-    //     document.getElementById("results").appendChild = vineresults;
-    // }
+    for (var i = 0; i < results.length; i++) {
+        var vineresults = vineresults + "<video><source src=\"" + results[i].url + "\"type=\"video/mp4\"></video><br>";
+        console.log(results[i].url);
+        document.getElementById("results").innerHTML = vineresults;
+    }
 }
