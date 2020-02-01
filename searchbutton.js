cbb4e31a0499c694485838bb40e53ada54de8d00
Jessica Miyake <43479962+jessicamiyake@users.noreply.github.com>
2020-02-01 14:59:00 -0500

    Update searchbutton.js

--- searchbutton.js
+++ searchbutton.js
@@ -1,5 +1,5 @@
 function search() {
     var input = document.getElementById('searchtext');
     var results = [];
-    document.getElementById("results").innerHTML = "<video><source src=\"videos/junior.mp4\" type=\"video/mp4\"></video>";
+    document.getElementById("results").innerHTML = "<video controls><source src=\"videos/junior.mp4\" type=\"video/mp4\"></video>";
 }
