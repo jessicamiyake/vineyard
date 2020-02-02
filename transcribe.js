88a6568658028e6ced02c698b355ff2269a27716
Rachel Zhang <34202146+rachelzhang98@users.noreply.github.com>
2020-02-02 00:33:00 -0500

    works now

--- transcribe.js
+++ transcribe.js
@@ -1,28 +1,56 @@
-/*// TODO:
-- get video from url - should not be hard but i cant figure it
-- extract transcription text
+/*
+This takes videos from /videos and transcribes them in transcripts.txt.
+Stores all videos and transcripts in s3 bucket specified below.
+Each video takes 2 lines: video file name on first line, transcription on second.
 */
 
 var AWS = require('aws-sdk'); //import aws sdk for nodejs/javascript
-AWS.config.update({region:'us-east-1'});
 var fs = require('fs');
-//var uuid = require('node-uuid'); //creates unique bucket id
+var path = require('path');
+const fetch = require("node-fetch");
 
+
+AWS.config.update({region:'us-east-1'});
 var s3 = new AWS.S3(); //new s3 client
+var transcribeservice = new AWS.TranscribeService({apiVersion: '2017-10-26'});
 
-//var bucketName = 'node-sdk-sample-' + uuid.v4();
-var bucketName = "node-sdk-sample-da9bc89e-a099-498d-b3d9-fb4cdd48aa7d";
-var keyName = "/videos/junior.mp4"; //name of file in local directory
-var filename = "/videos/junior.mp4"; //desired name of file on s3
-var mediaFormat = "mp4";
-var jobName = "transcription_job" + Math.random();
+  //var bucketName = 'node-sdk-sample-' + uuid.v4();
+
+const bucketName = "node-sdk-sample-da9bc89e-a099-498d-b3d9-fb4cdd48aa7d";
+const folder = './videos/';
+const mediaFormat = "mp4";
+const destination = 'transcripts.txt';
+var fileList = [];
+var vines = [];
+
+  //var transcripts = [];
+
+  //create_bucket(bucketName);
+  //upload_audio(filename, bucketName, keyName);
+  //transcribe(keyName, bucketName, mediaFormat, jobName);
+  //givePublicAccess(keyName);
+  //getTranscription(keyName, bucketName);
+
+  readFromDir(folder, bucketName, fileList);
+  fileList.forEach(file => {
+    var vine = {name: file, jobName: "transcription_job" + Math.random(), transcription: "", public: false};
+    vines.push(vine);
+    //console.log(vine.name);
+    //var jobName = "transcription_job" + Math.random();
+    transcribe(file, bucketName, mediaFormat, vine.jobName);
+  });
+
+  vines.forEach(vine => {
+    //console.log(vine.name);
+    //if (vine.finished) {
+    console.log("getting" + vine.jobName);
+    //givePublicAccess(vine.jobName+".json");
+    getTranscription(vine.jobName, vine.name);
+    //}
+  });
 
 
-//create_bucket(bucketName);
-upload_audio(filename, bucketName, keyName);
-transcribe(keyName, bucketName, mediaFormat, jobName);
-//givePublicAccess(keyName);
-//getTranscription(keyName, bucketName);
+/***********End Main *************************************************/
 
 /* Function creates a bucket under bucket_name. Must be unique. */
 function create_bucket(bucket_name) {
@@ -35,11 +63,22 @@ function create_bucket(bucket_name) {
  });
 }
 
+/*Uploads all audio in a folder to bucket*/
+function readFromDir(folder, bucket, fileList) {
+  fs.readdirSync(folder).forEach(file => {
+    var filePath = folder + file;
+    console.log(file);
+    upload_audio(filePath, bucket, file);
+    fileList.push(file);
+  });
+}
+
 /* Function uploads filename (stored locally) to bucketName under the name
 specified by object_name. */
 function upload_audio(filename, bucket, object_name) {
   var fileContent = fs.readFileSync(filename);
   var params = {
+  ACL: "public-read",
   Bucket: bucket,
   Key: object_name,
   Body: fileContent
@@ -53,8 +92,7 @@ function upload_audio(filename, bucket, object_name) {
 
 /* Function takes audio uploaded to bucketName under keyName and turns it
 into a transcription file in the same bucket. Output file is named jobName. */
-function transcribe(keyName, bucketName, mediaFormat, JobName) {
-  var transcribeservice = new AWS.TranscribeService({apiVersion: '2017-10-26'});
+function transcribe(keyName, bucketName, mediaFormat, jobName) {
   params = {
     LanguageCode: "en-US",
     Media: {
@@ -73,28 +111,84 @@ function transcribe(keyName, bucketName, mediaFormat, JobName) {
   });
 }
 
-/*function getFromBucket(bucketName, fileName){
-  var params = {
-    Bucket: bucketName,
-    Key: fileName
-  };
-  s3.getObject(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     {
-    console.log(data);
-    return data;
-  }      // successful response
+/* Gives file specified by key public read access. Will end up in infinite
+loop if there is an error. */
+function givePublicAccess(key) {
+    return new Promise((resolve, reject) => {
+      var params = {
+        ACL: 'public-read',
+        Bucket: bucketName,
+        Key: key
+       };
+      s3.putObjectAcl(params, function(err, data) {
+         if (err){
+           console.log(err, err.stack);
+           reject(err);
+           givePublicAccess(key);
+         }// an error occurred
+         else{ // successful response
+          console.log(data);
+          console.log("public access updated for" + key);
+           resolve(data);
+         }
 
-  });
+       });
+    })
 
-}*/
+}
 
-/*var params = {
-           TranscriptionJobName: jobName
-         };
-        transcribeservice.getTranscriptionJob(params, function(err, data) {
-           if (err) console.log(err, err.stack); // an error occurred
-           else{    // successful response
-               console.log(data);
-             }
-        });*/
+/* Waits for transcription to finish and gets the transcription text. Will end
+up in infinite loop if there is an error. */
+function getTranscription(jobName, videoName) {
+
+        //this.setState({transcriptionJobComplete: true});
+        //var currentComponent = this;
+        var params = {
+            TranscriptionJobName: jobName /* required */
+          };
+         transcribeservice.getTranscriptionJob(params, function(err, data) {
+            if (err) {
+              setTimeout(() => {
+                getTranscription(jobName, videoName);
+              }, 5000);
+            } // an error occurred
+            else{    // successful response
+                //console.log(data);
+                if(data.TranscriptionJob.TranscriptionJobStatus === 'IN_PROGRESS'){
+                  console.log("in progress")
+                  setTimeout(() => {
+                    getTranscription(jobName, videoName);
+                  }, 5000);
+                }
+                else if(data.TranscriptionJob.TranscriptionJobStatus === 'COMPLETED'){
+                  givePublicAccess(jobName + ".json");
+                  let url = data.TranscriptionJob.Transcript.TranscriptFileUri;
+                  console.log(url);
+                  //return url;
+                  getJson(url, videoName);
+                  //console.log(url);
+
+               }
+            }
+          });
+
+
+    }
+
+/* Helper function for getTranscription. Gets transcription from json in s3
+and appends it to the text file transcripts.txt */
+function getJson(url, videoName){
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+  console.log(out.results.transcripts[0].transcript);
+  fs.appendFile(destination, videoName + "\n"+out.results.transcripts[0].transcript+"\n", (err) => {
+    if (err) throw err;
+});
+}).catch(err => {
+  console.error(err);
+  setTimeout(() => {
+    getJson(url, videoName);
+  }, 1000);
+});
+}
